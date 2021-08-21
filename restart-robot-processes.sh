c24aedb2e71fed8a371f9f0a15376dd427cdc7cf
Francois Sunatori <francois.sunatori@vention.cc>
2021-08-21 11:04:09 -0400

    on ctrl-C don\'t close terminal tab

--- restart-robot-processes.sh
+++ restart-robot-processes.sh
@@ -1,8 +1,7 @@
-gnome-terminal --tab --title="rails" -- bash -ic "cd ~/Documents/vention_rails_docker && ./docker/run.sh start_run_with_host_assembler ~/Documents/vention_assembler/client/dist/ 6g"
+gnome-terminal --tab --title="rails" --command="bash -c 'endstart() { exec bash; }; trap endstart INT; cd ~/Documents/vention_rails_docker && ./docker/run.sh start_run_with_host_assembler ~/Documents/vention_assembler/client/dist/ 6g;'"
 
-gnome-terminal --tab --title="assembler" -- bash -ic "cd ~/Documents/vention_assembler/client && npm run dev"
+gnome-terminal --tab --title="assembler" --command="bash -c 'endstart() { exec bash; }; trap endstart INT; cd ~/Documents/vention_assembler/client && npm run dev;'"
 
-gnome-terminal --tab --title="execution-engine" -- bash -ic "cd ~/Documents/mm-execution-engine && npm run simulate-cad"
+gnome-terminal --tab --title="execution-engine" --command="bash -c 'endstart() { exec bash; }; trap endstart INT; cd ~/Documents/mm-execution-engine && npm run simulate-cad;'"
 
-gnome-terminal --tab --title="robot-packages" -- bash -ic "cd ~/Documents/scripts && bash npm-link-robot-packages.sh && bash npm-build-robot-packages.sh"
-
+gnome-terminal --tab --title="robot-packages" --command="bash -c 'endstart() { exec bash; }; trap endstart INT; cd ~/Documents/scripts && bash npm-link-robot-packages.sh && bash npm-build-robot-packages.sh;'"
