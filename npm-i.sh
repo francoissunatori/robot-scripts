799ac9127816e06fd30a21084fe22522657cd76a
Francois Sunatori <francois.sunatori@vention.cc>
2021-08-23 22:36:49 -0400

    add npm i

---
+++ npm-i.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cd ~/Documents/vention_assembler/client && npm i
+cd ~/Documents/vention_assembler/client/vse && npm i
+cd ~/Documents/mm-execution-engine && npm i
+cd ~/Documents/robot-components && npm i
+cd ~/Documents/machine-app-components && npm i
+
